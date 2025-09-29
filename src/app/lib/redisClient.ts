4efc2885db5f3c8a71a622e92194abcc5ccb8455
Shwetanshu-Bhatt <shwetanshubhatt@.com>
2025-09-29 20:11:44 +0530

    teting api

--- src/app/lib/redisClient.ts
+++ src/app/lib/redisClient.ts
@@ -1,19 +1,19 @@
+import { RedisClientType } from 'redis';
+import VercelKV from '@vercel/kv';
 import { createClient as createRedisClient } from 'redis';
 
-let redis: any;
+let redis: RedisClientType | typeof VercelKV;
 
 if (process.env.VERCEL) {
-  redis = createRedisClient({
-    url: process.env.KV_REST_API_URL,
-    password: process.env.KV_REST_API_TOKEN,
-  });
+  // On Vercel serverless: use the default KV export
+  redis = VercelKV; // just the default export
 } else {
+  // Local development with Upstash Redis
   redis = createRedisClient({
     url: process.env.UPSTASH_REDIS_REST_URL,
     password: process.env.UPSTASH_REDIS_REST_TOKEN,
   });
+  redis.connect().catch(console.error);
 }
-
-redis.connect().catch(console.error);
 
 export default redis;
