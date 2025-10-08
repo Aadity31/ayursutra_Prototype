58f76a4a442130ad6a6967116a815b3592baf97f
Shwetanshu-Bhatt <shwetanshubhatt@.com>
2025-10-08 14:06:27 +0530

    changes in hero tsx

--- src/app/doctor/graph/page.tsx
+++ src/app/doctor/graph/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React from "react";
-import Navbar from "../../components/doctor/Navbar";
-import Sidebar from "../../components/doctor/Sidebar";
-import GraphCard from "../../components/doctor/GraphCard";
+// import Navbar from "../../components/doctor/Navbar";
+// import Sidebar from "../../components/doctor/Sidebar";
+// import GraphCard from "../../components/doctor/GraphCard";
 
 const graphsData = [
   { title: "Active Patients", value: 120, color: "from-green-400 to-green-600" },
@@ -15,21 +15,21 @@ const GraphsPage: React.FC = () => {
   return (
     <div className="flex min-h-screen bg-gray-50">
       {/* Sidebar */}
-      <Sidebar />
+      {/* <Sidebar /> */}
 
       {/* Main content */}
       <div className="flex-1 flex flex-col">
         {/* Navbar */}
-        <Navbar />
+        {/* <Navbar /> */}
 
         {/* Page Content */}
         <div className="p-6 space-y-6">
           <h2 className="text-2xl font-bold mb-4">Graphs</h2>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {graphsData.map((graph, idx) => (
+            {/* {graphsData.map((graph, idx) => (
               <GraphCard key={idx} {...graph} />
-            ))}
+            ))} */}
           </div>
         </div>
       </div>
